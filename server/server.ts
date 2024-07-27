6676f4414a28d48983fec95eaedb2268e6f6e2f9
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-07-27 14:30:04 +1200

    enable cors

--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,22 @@ import { join } from 'node:path'
 import express from 'express'
 import * as Path from 'node:path'
 import * as URL from 'node:url'
-
+import cors from 'cors'
 import api from './routes/api.ts'
 
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
 const server = express()
+
+// CORS configuration
+const corsOptions = {
+  origin: 'http://example.com',  // Replace with your frontend origin
+  methods: ['GET', 'POST'],      // Allow specific methods
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Allow specific headers
+};
+// Use CORS middleware
+server.use(cors(corsOptions));
 
 server.use(express.json())
 server.use(express.static(join(__dirname, './public')))
